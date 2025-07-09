a979f2a789a5f749891f70b4f7c9f91a8d0031ff
Stef Smeets <stefsmeets@users.noreply.github.com>
2025-07-09 16:24:55 +0200

    Set up github action for pytest (#13)

--- python/tests/examples_test.py
+++ python/tests/examples_test.py
@@ -4,14 +4,20 @@ from pathlib import Path
 
 import pytest
 
+from pspython.pspyinstruments import discover_instruments
+
 ROOT_DIR = Path(__file__).parents[1]
 EXAMPLES = list(ROOT_DIR.glob('*Example*.py'))
 
+requires_instrument = pytest.mark.skipif(
+    not discover_instruments(), reason='Needs connected instrument.'
+)
+
 
 @pytest.mark.parametrize('path', EXAMPLES)
+@requires_instrument
 def test_examples(path: Path):
-    assert path.exists()
-    print(path)
+    assert path.exists(), f'Missing {path}'
 
     ret = sp.run([sys.executable, path])
 

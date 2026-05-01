0c716759e705781c99f90552ff6a945ac0a8d38d
Stef Smeets <stefsmeets@users.noreply.github.com>
2026-05-01 13:46:04 +0200

    Use EILevel for MSP methods (#351)

--- python/src/pypalmsens/_methods/levels.py
+++ python/src/pypalmsens/_methods/levels.py
@@ -131,8 +131,8 @@ class ILevel(BaseModel):
 
         return use_limit_potential_min or use_limit_potential_max
 
-    def to_psobj(self) -> PalmSens.Techniques.ELevel:
-        obj = PalmSens.Techniques.ELevel()
+    def to_psobj(self) -> PalmSens.Techniques.EILevel:
+        obj = PalmSens.Techniques.EILevel()
 
         obj.Level = self.level
         obj.Duration = self.duration
@@ -152,7 +152,7 @@ class ILevel(BaseModel):
         return obj
 
     @classmethod
-    def from_psobj(cls, psobj: PalmSens.Techniques.ELevel):
+    def from_psobj(cls, psobj: PalmSens.Techniques.EILevel):
         """Construct ILevel dataclass from PalmSens.Techniques.ELevel object."""
         trigger_lines: list[Literal[0, 1, 2, 3]] = []
 

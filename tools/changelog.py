416cdbfc6b1fd74f77b0b5cae52e101f153bf6c1
Stef Smeets <stefsmeets@users.noreply.github.com>
2026-05-11 11:37:23 +0200

    Update release script (#363)

---
+++ tools/changelog.py
@@ -0,0 +1,120 @@
+from __future__ import annotations
+
+import subprocess as sp
+import sys
+from datetime import datetime
+from pathlib import Path
+from textwrap import dedent
+
+ROOT = Path(__file__).parents[1]
+
+
+def get_latest_tag(component: str = 'python'):
+    cmd = 'git tag --list --sort -creatordate'
+    p = sp.run(cmd, capture_output=True)
+    tags = p.stdout.decode().splitlines()
+    for line in tags:
+        if line.startswith(component):
+            latest = line
+            break
+    else:
+        raise ValueError(f'No tag matching {component!r} in tags: {tags}.')
+    return latest
+
+
+def get_changes_since_tag(tag: str):
+    cmd = f'git log {tag}..HEAD --reverse --oneline'
+    p = sp.run(cmd, capture_output=True)
+    lines = p.stdout.decode().splitlines()
+    lines = (line.split(maxsplit=1)[1] for line in lines)
+
+    out = []
+
+    for line in lines:
+        if '(#' not in line and not line.endswith(')'):
+            continue
+
+        if line.startswith('('):
+            index = line.index(')')
+            line = line[index + 2 :]
+
+        index = line.index('(#')
+        pr = line[index + 2 : -1]
+        line = line.replace(
+            f'#{pr}', f'[#{pr}](https://github.com/PalmSens/PalmSens_SDK/pull/{pr})'
+        )
+        out.append(f'- {line}')
+
+    return '\n'.join(out)
+
+
+def update_python(new_tag: str):
+    previous_tag = get_latest_tag()
+    changelog = get_changes_since_tag(previous_tag)
+
+    time = datetime.today()
+
+    TEMPLATE_CHANGELOG = dedent("""\
+    # PyPalmSens {new_version}
+
+    > :fontawesome-brands-github: <a href="https://github.com/PalmSens/PalmSens_SDK/releases/tag/{new_tag}">{new_tag}</a>
+    - :fontawesome-brands-python: <a href="https://pypi.org/project/pypalmsens/{new_version}">pypalmsens-{new_version}</a>
+    - :fontawesome-solid-calendar: {time}
+
+    ## What's changed
+
+    {changelog}
+    """)
+
+    TEMPLATE_GH_RELEASES = dedent(f"""\
+    PyPalmSens {new_tag} is now available on PyPi.
+
+    To upgrade: `pip install pypalmsens -U`.
+
+    For more information, see: https://dev.palmsens.com/python/latest/_attachments/releases/#pypalmsens-{new_tag.replace('.', '')}
+
+    ## What's changed
+
+    {changelog}
+
+    **Full Changelog**: https://github.com/PalmSens/PalmSens_SDK/compare/{previous_tag}...{new_tag}
+    """)
+
+    index_path = Path(ROOT / 'python' / 'docs' / 'zensical' / 'releases' / 'index.md')
+
+    lines = index_path.read_text().splitlines()
+
+    index = lines.index('// latest')
+
+    if new_version in lines[index + 1]:
+        print('Tag already exists, skipping')
+    else:
+        new_string = TEMPLATE_CHANGELOG.format(
+            new_version=new_version, new_tag=new_tag, time=time.date(), changelog=changelog
+        )
+        print(new_string)
+
+        lines.insert(index + 1, new_string)
+        index_path.write_text('\n'.join(lines) + '\n', encoding='UTF-8')
+
+        print(f'Tag added to {index_path.name}')
+
+    gh_releases = TEMPLATE_GH_RELEASES.format(
+        new_version=new_version, new_tag=new_tag, time=time.date(), changelog=changelog
+    )
+
+    with open('changelog-python.md', 'w') as f:
+        f.write(gh_releases)
+
+
+if __name__ == '__main__':
+    component = 'python'
+    args = sys.argv[1:]
+
+    assert len(args) == 2
+
+    previous_version, new_version = args
+    previous_tag = f'{component}-{previous_version}'
+    new_tag = f'{component}-{new_version}'
+
+    update_python(new_tag)
--- tools/prepare_release.py
+++ tools/prepare_release.py
@@ -1,11 +1,15 @@
+from __future__ import annotations
+
 import argparse
 import os
 import shutil
 import subprocess as sp
 from contextlib import contextmanager
+from dataclasses import dataclass
 from pathlib import Path
+from typing import Literal
 
-for exe in ("gh", "bump-my-version"):
+for exe in ('gh', 'bump-my-version'):
     assert shutil.which(exe)
 
 ROOT = Path(__file__).parents[1]
@@ -13,133 +17,161 @@ ROOT = Path(__file__).parents[1]
 PR_BODY = """\
 This PR prepares for a new release of the {sdk} SDK.
 
-- branch: `release-{tag}`
-- version: `{version}`
-- sdk: `{sdk}`
+- branch: `release-{sdk.tag}`
+- version: `{sdk.version}`
+- sdk: `{sdk.name}`
 """
 
 
-@contextmanager
-def work_directory(path: Path):
-    prev_cwd = Path.cwd().resolve()
-    try:
-        os.chdir(path)
-        yield
-    finally:
-        os.chdir(prev_cwd)
+@dataclass
+class SDK:
+    name: Literal['python', 'matlab', 'maui', 'wpf', 'labview', 'winforms']
+    version: str = ''
+
+    def old_version(self):
+        """Get current version."""
+        cmd = ['bump-my-version', 'show', 'current_version']
+
+        with self.chdir():
+            p = sp.run(cmd, capture_output=True)
+
+        return p.stdout.decode().strip()
+
+    def bump(self, component: Literal['major', 'minor', 'patch']) -> SDK:
+        cmd = ['bump-my-version', 'show', '--increment', component, 'new_version']
+
+        with self.chdir():
+            p = sp.run(cmd, capture_output=True)
+
+        new_version = p.stdout.decode().strip()
+        return SDK(self.name, new_version)
+
+    @property
+    def tag(self) -> str:
+        return f'{self.name}-{self.version}'
+
+    @property
+    def workdir(self) -> Path:
+        return ROOT / self.name
+
+    @contextmanager
+    def chdir(self):
+        prev_cwd = Path.cwd().resolve()
+        try:
+            os.chdir(self.workdir)
+            yield
+        finally:
+            os.chdir(prev_cwd)
 
 
-def announce(tag: str):
-    releases_path = Path(
-        ROOT, "docs", "sdk", "modules", "ROOT", "pages", "releases.adoc"
-    )
+def commit_file(path: str | Path, message: str):
+    path = str(Path)
+    sp.check_call(['git', 'add', f'{path}'])
+    sp.check_call(['git', 'commit', '-m', message])
+
+
+def update_releases(sdk: SDK, commit: bool = False):
+    releases_path = Path(ROOT, 'docs', 'sdk', 'modules', 'ROOT', 'pages', 'releases.adoc')
     assert releases_path.exists()
     lines = releases_path.read_text().splitlines()
 
-    index = lines.index("// latest")
+    index = lines.index('// latest')
 
-    if tag in lines[index + 1]:
-        print("Tag already exists, skipping")
+    if sdk.tag in lines[index + 1]:
+        print('Tag already exists, skipping')
     else:
         new_line = (
-            f"- https://github.com/palmsens/palmsens_sdk/releases/tag/{tag}[{tag}]"
+            f'- https://github.com/palmsens/palmsens_sdk/releases/tag/{sdk.tag}[{sdk.tag}]'
         )
         lines.insert(index + 1, new_line)
-        releases_path.write_text("\n".join(lines) + "\n", encoding="UTF-8")
-        print(f"Tag added to {releases_path.name}")
+        releases_path.write_text('\n'.join(lines) + '\n', encoding='UTF-8')
+        print(f'Tag added to {releases_path.name}')
 
-    sp.check_call(["git", "add", f"{releases_path}"])
-    sp.check_call(["git", "commit", "-m", "Updated release index"])
-
-
-def get_new_version(sdk: str, component: str) -> str:
-    workdir = ROOT / sdk
-
-    cmd = ["bump-my-version", "show", "--increment", component, "new_version"]
-
-    # breakpoint()
-
-    with work_directory(workdir):
-        p = sp.run(cmd, capture_output=True)
-
-    return p.stdout.decode().strip()
+    if commit:
+        commit_file(releases_path, message='Updated release index')
 
 
-def bump_version(sdk: str, version: str):
-    workdir = ROOT / sdk
-
-    with work_directory(workdir):
+def bump_version_to(sdk: SDK):
+    with sdk.chdir():
         sp.check_call(
             [
-                "bump-my-version",
-                "bump",
-                "--new-version",
-                version,
-                "patch",
-                "--commit",
+                'bump-my-version',
+                'bump',
+                '--new-version',
+                sdk.version,
+                'patch',
+                '--commit',
             ]
         )
 
-    print(f"Bumped {sdk} version to {version}")
+    print(f'Set {sdk.name} version to {sdk.version}')
 
 
-def prepare_pr(sdk: str, version: str, base_branch="main"):
-    sp.check_call(["git", "checkout", f"origin/{base_branch}"])
+def prepare_release_branch(sdk: SDK, base_branch: str) -> str:
+    sp.check_call(['git', 'checkout', f'origin/{base_branch}'])
 
-    release_branch = f"release-{tag}"
+    release_branch = f'release-{sdk.tag}'
 
     sp.run(
-        ["git", "checkout", "-b", release_branch, f"origin/{base_branch}"],
+        ['git', 'checkout', '-b', release_branch, f'origin/{base_branch}'],
         check=True,
     )
 
-    print(f"Created branch {release_branch}")
+    print(f'Created branch {release_branch}')
 
-    announce(tag)
-    bump_version(sdk, version)
+    return release_branch
 
+
+def push_branch_and_create_pr(sdk: SDK, base_branch: str, release_branch: str):
     sp.run(
-        ["git", "push", "origin", f"HEAD:{release_branch}", "--force"],
+        ['git', 'push', 'origin', f'HEAD:{release_branch}', '--force'],
         check=True,
     )
-    print(f"Pushed {release_branch}")
+    print(f'Pushed {release_branch}')
 
-    body = PR_BODY.format(version=version, sdk=sdk, tag=tag)
+    body = PR_BODY.format(version=sdk.version, sdk=sdk, tag=sdk.tag)
     print(body)
 
     sp.run(
         [
-            "gh",
-            "pr",
-            "create",
-            f"--base={base_branch}",
-            f"--head={release_branch}",
-            f"--title=Release {sdk}-{version}",
-            f"--body={body}",
-            "--draft",
+            'gh',
+            'pr',
+            'create',
+            f'--base={base_branch}',
+            f'--head={release_branch}',
+            f'--title=Release {sdk.tag}',
+            f'--body={body}',
+            '--draft',
         ],
         check=True,
     )
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("sdk", type=str)
-    parser.add_argument("--version", type=str, help="Set version.")
-    parser.add_argument(
-        "--bump", type=str, help="Major/minor/patch, overrides version."
-    )
+    parser.add_argument('sdk', type=str)
+    parser.add_argument('--version', type=str, help='Set version.')
+    parser.add_argument('--bump', type=str, help='Major/minor/patch, overrides version.')
     options = parser.parse_args()
 
     if options.bump:
-        version = get_new_version(sdk=options.sdk, component=options.bump)
+        sdk = SDK(options.sdk)
+        sdk = sdk.bump(component=options.bump)
     else:
-        version = options.version
+        sdk = SDK(options.sdk, options.version)
 
-    tag = f"{options.sdk}-{version}"
-    print(f"{tag=}")
+    print(f'New version: {sdk.tag=}')
 
-    prepare_pr(
-        sdk=options.sdk,
-        version=version,
-    )
+    base_branch = 'main'
+
+    release_branch = prepare_release_branch(sdk=sdk, base_branch=base_branch)
+    update_releases(sdk=sdk, commit=True)
+
+    if sdk.name == 'python':
+        import changelog
+
+        changelog.update_python(new_tag=sdk.tag)
+
+    bump_version_to(sdk)
+
+    push_branch_and_create_pr(sdk=sdk, release_branch=release_branch, base_branch=base_branch)

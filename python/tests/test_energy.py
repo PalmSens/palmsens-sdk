3d2d464c2e01b8fcfe71eb1a1f129f628e74a271
Stef Smeets <stefsmeets@users.noreply.github.com>
2026-06-04 14:37:32 +0200

    Fix tests (#395)

--- python/tests/test_energy.py
+++ python/tests/test_energy.py
@@ -170,4 +170,5 @@ def test_params_round_trip(method):
         ps.save_method_file(path, ms_params)
         new_params = ps.load_method_file(path)
 
-    assert new_params.script.splitlines() == params.render().splitlines()
+    # skip header/timestamp
+    assert new_params.script.splitlines()[4:] == params.render().splitlines()[4:]

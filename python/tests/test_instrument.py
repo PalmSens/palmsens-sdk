0f77f4e3b9978dbd5f1a42e784614548fa00dbf6
Stef Smeets <stef@palmsens.com>
2026-08-03 13:41:31 +0200

    Use StatusWhenIdle instead of COMM_MODE

--- python/tests/test_instrument.py
+++ python/tests/test_instrument.py
@@ -90,7 +90,7 @@ def test_firmware_warning_fail(cap):
 def test_connect():
     with ps.connect() as manager:
         assert isinstance(manager, ps.InstrumentManager)
-        assert str(manager._comm.ClientConnection._mode) == 'MODE_FAST'
+        assert not manager._comm.StatusWhenIdle
 
 
 @pytest.mark.instrument
@@ -122,7 +122,7 @@ def test_connect_serial_port_fail():
 async def test_connect_async():
     async with await ps.connect_async() as manager:
         assert isinstance(manager, ps.InstrumentManagerAsync)
-        assert str(manager._comm.ClientConnection._mode) == 'MODE_FAST'
+        assert not manager._comm.StatusWhenIdle
 
 
 @pytest.mark.instrument

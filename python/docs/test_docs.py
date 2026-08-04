f464b929e64b5ca546aa76574aae5fbe2de167d4
Stef Smeets <stef@palmsens.com>
2026-08-04 11:49:21 +0200

    Update measuring.md

--- python/docs/test_docs.py
+++ python/docs/test_docs.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from collections import defaultdict
 from pathlib import Path
 from typing import Any
 
@@ -9,13 +10,19 @@ from pytest_examples import CodeExample, EvalExample, find_examples
 docs_dir = Path(__file__).parent / 'zensical'
 
 
-MODULE_GLOBALS: dict[str, Any] = {}
+GROUP_MODULES: dict[str, dict[str, Any]] = defaultdict(dict)
 
 
-@pytest.mark.parametrize('example', list(find_examples(docs_dir / 'data.md')), ids=str)
+# @pytest.mark.parametrize('example', list(find_examples(docs_dir / 'data.md')), ids=str)
+@pytest.mark.parametrize('example', list(find_examples(docs_dir / 'measuring.md')), ids=str)
 def test_docstrings(example: CodeExample, eval_example: EvalExample):
+
+    group = example.path.name
+
+    d = GROUP_MODULES[group]
+
     eval_example.set_config(target_version='py310')
 
-    module_dict = eval_example.run_print_update(example, module_globals=MODULE_GLOBALS)
+    module_dict = eval_example.run_print_update(example, module_globals=d)
 
-    MODULE_GLOBALS.update(module_dict)
+    GROUP_MODULES[group] = module_dict

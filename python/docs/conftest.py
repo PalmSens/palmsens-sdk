e421e69ea3ff62fc1c57d990abab219905a894f4
Stef Smeets <stefsmeets@users.noreply.github.com>
2026-08-06 17:07:47 +0200

    Test comm_protocol.md

---
+++ python/docs/conftest.py
@@ -0,0 +1,32 @@
+import doctest
+from os import chdir, getcwd
+from shutil import rmtree
+from tempfile import mkdtemp
+
+import pytest
+from sybil import Sybil
+from sybil.parsers.doctest import DocTestParser
+from sybil.parsers.markdown import PythonCodeBlockParser
+
+
+@pytest.fixture(scope='module')
+def tempdir():
+    # there are better ways to do temp directories, but it's a simple example:
+    path = mkdtemp()
+    cwd = getcwd()
+    try:
+        chdir(path)
+        yield path
+    finally:
+        chdir(cwd)
+        rmtree(path)
+
+
+pytest_collect_file = Sybil(
+    parsers=[
+        DocTestParser(optionflags=doctest.ELLIPSIS),
+        PythonCodeBlockParser(future_imports=['print_function']),
+    ],
+    pattern='comm_protocol.md',
+    fixtures=['tempdir'],
+).pytest()

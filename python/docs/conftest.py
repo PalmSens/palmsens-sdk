ba68339e6cdb84a62a38d1ba2ec104ccb6ec0ab4
Stef Smeets <stef@palmsens.com>
2026-08-07 12:05:17 +0200

    Update data.md

--- python/docs/conftest.py
+++ python/docs/conftest.py
@@ -7,6 +7,7 @@ from tempfile import mkdtemp
 
 import pytest
 from sybil import Sybil
+from sybil.evaluators.doctest import NUMBER
 from sybil.parsers.doctest import DocTestParser
 from sybil.parsers.markdown import PythonCodeBlockParser
 
@@ -26,20 +27,26 @@ def tempdir():
 
 pytest_collect_file = Sybil(
     parsers=[
-        DocTestParser(optionflags=doctest.ELLIPSIS),
-        PythonCodeBlockParser(future_imports=['print_function']),
+        DocTestParser(
+            optionflags=doctest.ELLIPSIS
+            | doctest.NORMALIZE_WHITESPACE
+            | doctest.IGNORE_EXCEPTION_DETAIL
+        ),
+        PythonCodeBlockParser(
+            future_imports=['print_function'],
+            doctest_optionflags=NUMBER,
+        ),
     ],
     patterns=[
         # 'comm_protocol.md',
-        # 'index.md',
-        'circuit_fitting.md',
-        # 'comm_protocol.md',
-        # 'data.md',
+        # 'circuit_fitting.md',
+        'data.md',
         # 'events.md',
         # 'examples.md',
         # 'files.md',
         # 'filesystem.md',
         # 'installation.md',
+        # 'index.md',
     ],
     # fixtures=['tempdir'],
 ).pytest()

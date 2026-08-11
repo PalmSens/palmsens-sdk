95baf6265dc31f6d23709b5a13a3039d08ad8a05
Stef Smeets <stef@palmsens.com>
2026-08-11 15:25:56 +0200

    Update plot

--- application_notes/methodscript_app/app.py
+++ application_notes/methodscript_app/app.py
@@ -14,53 +14,56 @@ from threading import Thread
 
 import altair as alt
 import pandas as pd
-import pypalmsens as ps
 import streamlit as st
-from pypalmsens.data import CallbackData, Curve, Measurement
-from pypalmsens.energy import experimental_BatteryCycling
 from streamlit.runtime.scriptrunner import add_script_run_ctx, get_script_run_ctx
 
+import pypalmsens as ps
+from pypalmsens.data import CallbackData, Curve, Measurement
+from pypalmsens.energy import experimental_BatteryCycling
+
 st.set_page_config(
-    page_title="Battery Cycling",
-    page_icon=":material/battery_android_frame_bolt:",
+    page_title='Battery Cycling',
+    page_icon=':material/battery_android_frame_bolt:',
     menu_items={
-        "Get Help": "https://palmsens.com/contact",
-        "Report a bug": "https://github.com/palmsens/palmsens_sdk/issues",
+        'Get Help': 'https://palmsens.com/contact',
+        'Report a bug': 'https://github.com/palmsens/palmsens_sdk/issues',
     },
-    layout="wide",
-    initial_sidebar_state="expanded",
+    layout='wide',
+    initial_sidebar_state='expanded',
 )
 
 
 st.logo(
-    "https://dev.palmsens.com/python/latest/_attachments/assets/banner.svg",
-    link="https://dev.palmsens.com/python/latest",
-    size="large",
+    'https://dev.palmsens.com/python/latest/_attachments/assets/banner.svg',
+    link='https://dev.palmsens.com/python/latest',
+    size='large',
 )
 
 
 SESSION = st.session_state
 
-if "manager" not in SESSION:
+if 'manager' not in SESSION:
     SESSION.manager = None
 
-if "is_measuring" not in SESSION:
+if 'is_measuring' not in SESSION:
     SESSION.is_measuring = False
 
-if "thread" not in SESSION:
+if 'thread' not in SESSION:
     SESSION.thread = None
 
-if "start" not in SESSION:
+if 'start' not in SESSION:
     SESSION.start = False
 
-if "measurement" not in SESSION:
+if 'measurement' not in SESSION:
     SESSION.measurement = None
 
-if "curves" not in SESSION:
+if 'curves' not in SESSION:
     SESSION.curves = {}
     SESSION.curves_metadata = {}
-    SESSION.i_curves = []
-    SESSION.e_curves = []
+    SESSION.i_curve_ids = set()
+    SESSION.e_curve_ids = set()
+    SESSION.i_values = []
+    SESSION.e_values = []
 
 
 class MethodThread(Thread):
@@ -75,31 +78,34 @@ class MethodThread(Thread):
 
         SESSION.curves.clear()
         SESSION.curves_metadata.clear()
-        SESSION.i_curves.clear()
-        SESSION.e_curves.clear()
+        SESSION.i_curve_ids.clear()
+        SESSION.e_curve_ids.clear()
+        SESSION.i_values.clear()
+        SESSION.e_values.clear()
 
         def on_curve_begin(curve: Curve):
             if curve.title.startswith('CP: t vs E'):
-            if curve.title.startswith("CP: t vs E"):
-                SESSION.e_curves.append(curve.id)
-            elif curve.title.startswith("CP: t vs i"):
-                SESSION.i_curves.append(curve.id)
-            elif curve.title.startswith("Unknown"):
+                SESSION.e_curve_ids.add(curve.id)
+            elif curve.title.startswith('CP: t vs i'):
+                SESSION.i_curve_ids.add(curve.id)
+            elif curve.title.startswith('Unknown'):
                 pass
 
             SESSION.curves_metadata[curve.id] = curve.metadata()
             SESSION.curves[curve.id] = []
 
         def on_data(data: CallbackData):
-            for row in zip(data.x_array[data.start :], data.y_array[data.start :]):
-                SESSION.curves[data.id].append(row)
+            for x, y in zip(data.x_array[data.start :], data.y_array[data.start :]):
+                if data.id in SESSION.i_curve_ids:
+                    SESSION.i_values.append({'x': x, 'y': y, 'id': data.id})
+                elif data.id in SESSION.e_curve_ids:
+                    SESSION.e_values.append({'x': x, 'y': y, 'id': data.id})
 
         SESSION.manager.on_curve_begin(on_curve_begin)
         SESSION.manager.on_curve_new_data(on_data)
         SESSION.manager.on_receive_message(self.set_status_message)
 
         measurement = SESSION.manager.measure(method)
-
 
         self.measurement = measurement
 
@@ -109,88 +115,90 @@ class MethodThread(Thread):
 
 def configure_method_widget() -> experimental_BatteryCycling:
     """Configure battery cycling method via widget."""
-    st.header("Method parameters")
+    st.header('Method parameters')
 
     potential_max = st.number_input(
-        "Potential Max (mV)",
+        'Potential Max (mV)',
         value=4300,
-        help="Maximum potential to charge to (units: mV).",
+        help='Maximum potential to charge to (units: mV).',
         disabled=SESSION.thread is not None,
     )
 
     current_min = st.number_input(
-        "Current Min (μA)",
+        'Current Min (μA)',
         value=5,
-        help="Minimum current to stop the CV charge step (units: μA).",
+        help='Minimum current to stop the CV charge step (units: μA).',
         disabled=SESSION.thread is not None,
     )
 
     potential_min = st.number_input(
-        "Potential Min (mV)",
+        'Potential Min (mV)',
         value=2500,
-        help="Minimum potential to discharge to (units: mV).",
+        help='Minimum potential to discharge to (units: mV).',
         disabled=SESSION.thread is not None,
     )
 
     current_charge = st.number_input(
-        "Current Charge (μA)",
+        'Current Charge (μA)',
         value=100,
-        help="Constant current to charge with (units: μA).",
+        help='Constant current to charge with (units: μA).',
         disabled=SESSION.thread is not None,
     )
 
     current_discharge = st.number_input(
-        "Current Discharge (μA)",
+        'Current Discharge (μA)',
         value=-100,
-        help="Constant current to discharge with (units: μA).",
+        help='Constant current to discharge with (units: μA).',
         disabled=SESSION.thread is not None,
     )
 
     cycles = st.number_input(
-        "Cycles",
+        'Cycles',
         value=1,
-        help="Number of charge and discharge cycles.",
+        help='Number of charge and discharge cycles.',
         disabled=SESSION.thread is not None,
     )
 
     interval = st.number_input(
-        "Interval (s)",
-        value=1,
-        help="Interval time of each measurement point (units: s).",
+        'Interval (s)',
+        value=1.0,
+        step=0.1,
+        min_value=0.001,
+        help='Interval time of each measurement point (units: s).',
         disabled=SESSION.thread is not None,
     )
 
     max_time = st.number_input(
-        "Max Time (s)",
+        'Max Time (s)',
         value=10,
-        help="Maximum duration of each step (if the cut-off is not met) (units: s).",
+        help='Maximum duration of each step (if the cut-off is not met) (units: s).',
         disabled=SESSION.thread is not None,
     )
 
     delta_v = st.number_input(
-        "Delta V (μV)",
+        'Delta V (μV)',
         value=100,
         min_value=0,
-        help="Minimum potential variation required for plotting data in CC steps (units: μV).",
+        help='Minimum potential variation required for plotting data in CC steps (units: μV).',
         disabled=SESSION.thread is not None,
     )
 
     delta_i = st.number_input(
-        "Delta I (nA)",
+        'Delta I (nA)',
         value=500,
         min_value=0,
-        help="Minimum current variation reuqired for plotting data in the CV step (units: nA).",
+        help='Minimum current variation reuqired for plotting data in the CV step (units: nA).',
         disabled=SESSION.thread is not None,
     )
 
     delta_t = st.number_input(
-        "Delta T (ms)",
+        'Delta T (ms)',
         value=100,
-        help="Maximum time without plotting data (units: ms).",
+        help='Maximum time without plotting data (units: ms).',
         disabled=SESSION.thread is not None,
     )
 
-    return ps.energy.experimental_BatteryCycling(
+    method = ps.energy.experimental_BatteryCycling(
         potential_max=potential_max,
         current_min=current_min,
         potential_min=potential_min,
@@ -204,27 +212,29 @@ def configure_method_widget() -> experimental_BatteryCycling:
         delta_t=delta_t,
     )
 
+    return method
+
 
 def show_methodscript_widget(methodscript: ps.MethodScript) -> None:
     """Show widget with generated methodscript."""
     c1, c2, _ = st.columns(3)
 
     _ = c1.download_button(
-        "Save as script (.mscr)",
+        'Save as script (`.mscr`)',
         data=methodscript.script,
-        file_name="battery_cycler.mscr",
-        mime="text/plain",
-        icon=":material/download:",
-        width="stretch",
+        file_name='battery_cycler.mscr',
+        mime='text/plain',
+        icon=':material/download:',
+        width='stretch',
         disabled=SESSION.is_measuring,
     )
     _ = c2.download_button(
-        "Save as method file (.psmethod)",
+        'Save as method file (`.psmethod`)',
         data=methodscript._serialize(),
-        file_name="battery_cycler.psmethod",
-        mime="text/plain",
-        icon=":material/download:",
-        width="stretch",
+        file_name='battery_cycler.psmethod',
+        mime='text/plain',
+        icon=':material/download:',
+        width='stretch',
         disabled=SESSION.is_measuring,
     )
 
@@ -240,36 +250,36 @@ def connect_to_device_widget(c1, c2):
         instruments = ps.discover()
 
         if not instruments:
-            st.warning("No devices were discovered.")
+            st.warning('No devices were discovered.')
             st.stop()
 
         options = {instrument.name: instrument for instrument in instruments}
 
         name = c1.selectbox(
-            "Instrument",
+            'Instrument',
             options,
-            placeholder="Select instrument",
+            placeholder='Select instrument',
             index=0,
-            label_visibility="collapsed",
+            label_visibility='collapsed',
         )
 
         instrument = options[name]
 
         c2.metric(
-            label="device",
+            label='device',
             value=instrument.name,
-            delta="disconnected",
-            delta_color="red",
-            delta_arrow="off",
+            delta='disconnected',
+            delta_color='red',
+            delta_arrow='off',
         )
 
         if c1.button(
-            "Connect",
-            type="primary",
-            icon=":material/add_link:",
-            width="stretch",
+            'Connect',
+            type='primary',
+            icon=':material/add_link:',
+            width='stretch',
         ):
-            with c1.spinner("Connecting..."):
+            with c1.spinner('Connecting...'):
                 try:
                     SESSION.manager = ps.connect(instrument=instrument)
                 except ConnectionError as e:
@@ -281,17 +291,17 @@ def connect_to_device_widget(c1, c2):
         st.stop()
     else:
         c2.metric(
-            label="device",
+            label='device',
             value=SESSION.manager.instrument.name,
-            delta="connected",
-            delta_color="green",
-            delta_arrow="off",
+            delta='connected',
+            delta_color='green',
+            delta_arrow='off',
         )
 
         if c1.button(
-            "Disconnect",
-            width="stretch",
-            icon=":material/link_off:",
+            'Disconnect',
+            width='stretch',
+            icon=':material/link_off:',
         ):
             SESSION.manager.disconnect()
             SESSION.manager = None
@@ -304,25 +314,24 @@ def start_measurement_widget(c1, c2, *, method: experimental_BatteryCycling):
 
     if not SESSION.thread:
         if c1.button(
-            "Start measurement",
-            type="primary",
-            icon=":material/play_circle:",
-            width="stretch",
+            'Start measurement',
+            type='primary',
+            icon=':material/play_circle:',
+            width='stretch',
         ):
-            with c1.spinner("Starting measurement..."):
+            with c1.spinner('Starting measurement...'):
                 SESSION.thread = MethodThread(method=method)
                 add_script_run_ctx(SESSION.thread, get_script_run_ctx())
                 SESSION.thread.start()
                 SESSION.is_measuring = True
-                print("thread started!", SESSION.is_measuring)
 
             st.rerun()
     else:
         if c1.button(
-            "Abort measurement",
-            type="primary",
-            icon=":material/cancel:",
-            width="stretch",
+            'Abort measurement',
+            type='primary',
+            icon=':material/cancel:',
+            width='stretch',
         ):
             SESSION.thread.join()
             SESSION.manager.abort()
@@ -343,27 +352,26 @@ def live_measurement_widget(method: experimental_BatteryCycling):
         update_every = 1
 
         while SESSION.thread.is_alive():
-            print("pulse")
             time.sleep(update_every)
 
-            curve_no = len(SESSION.i_curves)
+            curve_no = len(SESSION.i_curve_ids)
             progress_bar.progress(
                 curve_no / method.cycles,
                 text=SESSION.thread.message,
             )
 
-            if SESSION.i_curves:
-                i_chart.altair_chart(make_chart(curve_id=SESSION.i_curves[-1]))
+            if SESSION.i_values:
+                i_chart.altair_chart(make_chart2(values=SESSION.i_values))
 
-            if SESSION.e_curves:
-                e_chart.altair_chart(make_chart(curve_id=SESSION.e_curves[-1]))
+            if SESSION.e_values:
+                e_chart.altair_chart(make_chart2(values=SESSION.e_values))
 
         SESSION.thread.join()
         SESSION.measurement = SESSION.thread.measurement
         SESSION.thread = None
         SESSION.is_measuring = False
 
-        progress_bar.progress(1.0, text="Measurement finished!")
+        progress_bar.progress(1.0, text='Measurement finished!')
 
         st.rerun()
 
@@ -373,11 +381,11 @@ def show_measurement_widget():
     c1, c2 = st.columns(2)
 
     for curve in SESSION.measurement.curves:
-        if curve.title == "Unknown: cyc vs Qpass":
-            title = "Charge"
+        if curve.title == 'Unknown: cyc vs Qpass':
+            title = 'Charge'
             col = c1
-        elif curve.title == "Unknown: cyc vs Qpass 1":
-            title = "Discharge"
+        elif curve.title == 'Unknown: cyc vs Qpass 1':
+            title = 'Discharge'
             col = c2
         else:
             continue
@@ -385,53 +393,57 @@ def show_measurement_widget():
         if len(curve) <= 1:
             continue
 
-        source = pd.DataFrame({"x": curve.x_array, "y": curve.y_array})
+        source = pd.DataFrame({'x': curve.x_array, 'y': curve.y_array})
 
         chart = (
             alt.Chart(source, title=title)
             .mark_line()
             .encode(
-                alt.X("x").title(f"{curve.x_label} / {curve.x_unit}"),
-                alt.Y("y").title(f"{curve.y_label} / {curve.y_unit}"),
+                alt.X('x').title(f'{curve.x_label} / {curve.x_unit}'),
+                alt.Y('y').title(f'{curve.y_label} / {curve.y_unit}'),
             )
         ).interactive()
         col.altair_chart(chart)
 
-    if SESSION.i_curves:
-        c1.altair_chart(make_chart(curve_id=SESSION.i_curves[-1]).interactive())
+    if SESSION.i_curve_ids:
+        c1.altair_chart(make_chart2(values=SESSION.i_values).interactive())
 
-    if SESSION.e_curves:
-        c2.altair_chart(make_chart(curve_id=SESSION.e_curves[-1]).interactive())
+    if SESSION.e_curve_ids:
+        c2.altair_chart(make_chart2(values=SESSION.e_values).interactive())
 
     if SESSION.measurement:
         c1, _, _ = st.columns(3)
 
+        if c1.button('Save to disk'):
+            ps.save_measurement('battery_cycler.pssession', SESSION.measurement)
+
         _ = c1.download_button(
-            "Save measurement data (.pssession) ",
-            data=b"Not implemented yet =(",
-            file_name="battery_cycler.pssession",
-            mime="text/plain",
-            icon=":material/download:",
-            width="stretch",
+            'Save measurement data (`.pssession`)',
+            data=b'Not implemented yet =(',
+            file_name='battery_cycler.pssession',
+            mime='text/plain',
+            icon=':material/download:',
+            width='stretch',
         )
 
 
-def make_chart(curve_id: int) -> alt.Chart:
-    """Make altair chart chart for curve id."""
-    data = SESSION.curves[curve_id]
-    metadata = SESSION.curves_metadata[curve_id]
+def make_chart2(values: list[dict[str, float]]) -> alt.Chart:
+    """Make altair chart for curve id."""
+    first_row = values[0]
+    metadata = SESSION.curves_metadata[first_row['id']]
 
     x_label, y_label = metadata.labels
     x_unit, y_unit = metadata.units
 
-    source = pd.DataFrame(data, columns=metadata.columns)
+    source = alt.Data(values=values)
 
     chart = (
         alt.Chart(source, title=metadata.title)
         .mark_line()
         .encode(
-            alt.X("x").title(f"{x_label} / {x_unit}"),
-            alt.Y("y").title(f"{y_label} / {y_unit}"),
+            alt.X('x:Q').title(f'{x_label} / {x_unit}'),
+            alt.Y('y:Q').title(f'{y_label} / {y_unit}'),
+            color='id:N',
         )
     )
 
@@ -439,14 +451,14 @@ def make_chart(curve_id: int) -> alt.Chart:
 
 
 def main():
-    st.title("Battery Cycling")
+    st.title('Battery Cycling')
 
     with st.sidebar:
         method = configure_method_widget()
 
     methodscript = method.to_methodscript()
 
-    with st.expander("Click to show generated MethodSCRIPT"):
+    with st.expander('Click to show generated MethodSCRIPT'):
         show_methodscript_widget(methodscript)
 
     c1, c2, _ = st.columns(3)
@@ -455,15 +467,13 @@ def main():
 
     start_measurement_widget(c1, c2, method=method)
 
-    st.space(size="small")
+    st.space(size='small')
 
     live_measurement_widget(method)
-
-    print(SESSION.is_measuring, SESSION.measurement)
 
     if (not SESSION.is_measuring) and SESSION.measurement:
         show_measurement_widget()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()

287edb0849872f478081e9a150bd826cecf9b91b
Stef Smeets <stefsmeets@users.noreply.github.com>
2026-08-17 15:08:11 +0200

    Remove deprecated functions (#455)

--- python/src/pypalmsens/settings.py
+++ python/src/pypalmsens/settings.py
@@ -2,9 +2,6 @@
 
 from __future__ import annotations
 
-import warnings
-from typing import Any
-
 from ._methods.levels import (
     ELevel,
     ILevel,
@@ -29,27 +26,6 @@ from ._methods.settings import (
     Pretreatment,
     VersusOCP,
 )
-
-
-def __getattr__(name: str) -> Any:
-    if name in (
-        'AllowedCurrentRanges',
-        'AllowedDeviceState',
-        'AllowedMethods',
-        'AllowedPotentialRanges',
-        'AllowedReadingStatus',
-        'AllowedTimingStatus',
-    ):
-        warnings.warn(
-            f"{name!r} has moved, use 'pypalmsens.types.{name}'",
-            DeprecationWarning,
-            stacklevel=2,
-        )
-        from . import types
-
-        return getattr(types, name)
-    raise AttributeError(f'module {__name__!r} has no attribute {name!r}')
-
 
 __all__ = [
     'BiPot',

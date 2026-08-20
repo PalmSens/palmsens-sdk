8828a1c7ba1c5fc8b9bed564669da2e0ba6996dd
Stef Smeets <stefsmeets@users.noreply.github.com>
2026-08-20 14:08:57 +0200

    Update PalmSens-stubs for type checking (#459)

--- python/build_type_stubs.ps1
+++ python/build_type_stubs.ps1
@@ -1,4 +1,4 @@
-# git clone https://github.com/bph-tuwien/pythonnet-stub-generator
+# git clone https://github.com/mlidbom/pythonnet-stub-generator
 
 dotnet run --project pythonnet-stub-generator/csharp/PythonnetStubTool -- --target-dlls src/pypalmsens/_libpalmsens/win/PalmSens.Core.dll --dest-path tmp
 mv tmp/PalmSens/* src/PalmSens-stubs/

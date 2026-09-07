103e5349de0db4eee07c6968c33beca0bf83c605
Stef Smeets <stefsmeets@users.noreply.github.com>
2026-09-07 11:38:26 +0200

    Bump version: 1.11.0 → 2.0.0

--- python/src/pypalmsens/__init__.py
+++ python/src/pypalmsens/__init__.py
@@ -15,7 +15,7 @@ from __future__ import annotations
 from . import _libpalmsens
 
 __sdk_version__: str = _libpalmsens.load()
-__version__ = '1.11.0'
+__version__ = '2.0.0'
 
 from . import (
     corrosion,
